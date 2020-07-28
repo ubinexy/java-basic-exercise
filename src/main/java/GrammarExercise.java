f3fd259e4c72e675dd5aa59e8ac7eb147aa1b1fb
ubinexy <ubinexy@gmail.com>
2020-07-28 21:56:15 +0800

    Exercise: finished

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,4 +1,6 @@
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GrammarExercise {
     public static void main(String[] args) {
@@ -6,13 +8,36 @@ public class GrammarExercise {
         String firstWordList = "";
         String secondWordList = "";
 
+        Scanner sc1 = new Scanner( System.in );
+        System.out.println("First wordlist:");
+        firstWordList = sc1.next();
+        Scanner sc2 = new Scanner( System.in );
+        System.out.println("Second wordlist:");
+        secondWordList = sc2.next();
+
+
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
-
+        System.out.println(result.toString());
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        if(firstWordList.chars().anyMatch(x->!(x==44||(64<x&&x<91)||(96<x&&x<123))))
+            throw new RuntimeException("input not valid");
+
+        if(secondWordList.chars().anyMatch(x->!(x==44||(64<x&&x<91)||(96<x&&x<123))))
+            throw new RuntimeException("input not valid");
+
+        List<String> first = Stream.of(firstWordList.split(",")).map(String::toUpperCase).collect(Collectors.toList());
+        List<String> second = Stream.of(secondWordList.split(",")).map(String::toUpperCase).collect(Collectors.toList());
+
+        if(first.contains("") || second.contains("")) {
+            throw new RuntimeException("input not valid");
+        }
+        first.retainAll(second);
+
+        return first.stream().map(word -> Stream.of(word.split("")).reduce("",(a, b)->a+" "+b).trim()
+        ).distinct().sorted().collect(Collectors.toList());
     }
 }
